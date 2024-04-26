5f064ddd5c23da51aac9402f57f62710faff163e
SureshLizzy <sureshforjira@gmail.com>
2024-04-26 15:22:55 +0530

    Added new method

--- src/test/java/com/tcs/Class1.java
+++ src/test/java/com/tcs/Class1.java
@@ -8,4 +8,14 @@ public class Class1 {
 	public void method2() {
 		System.out.println("method2");
 	}
+	public void method5() {
+		System.out.println("method5");
+	}
+	public void method3() {
+		System.out.println("method3");
+	}
+	public void method4() {
+		System.out.println("method4");
+	}
+	
 }
