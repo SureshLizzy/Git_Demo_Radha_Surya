c17621b6cb3085796ea1fe2a53c31599b75dc25e
SureshLizzy <sureshforjira@gmail.com>
2024-04-26 15:09:57 +0530

    New update

--- src/test/java/com/tcs/Class1.java
+++ src/test/java/com/tcs/Class1.java
@@ -8,4 +8,10 @@ public class Class1 {
 	public void method2() {
 		System.out.println("method2");
 	}
+	public void method3() {
+		System.out.println("method3");
+	}
+	public void method4() {
+		System.out.println("method4");
+	}
 }
